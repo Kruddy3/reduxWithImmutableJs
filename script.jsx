9a1b598bac4fe63535b20932cebbebda8070fb5e
thestraded <thestraded@gmail.com>
2018-02-14 11:30:01 -0500

    get passed in an array and changes the currently viewing in the store

--- script.jsx
+++ script.jsx
@@ -1,4 +1,4 @@
-const counter = (state = {currentViewing:'',toDoLists:{}}, action) => {
+const todoListManager = (state ={currentViewing:'', toDoLists:{todoname:[],todoname2:[]}}, action) => {
   var immutableStore = Immutable.fromJS(state)
   switch (action.type) {
     // add TodoList
@@ -35,12 +35,125 @@ const counter = (state = {currentViewing:'',toDoLists:{}}, action) => {
       return immutableStore
           .set('currentViewing' , action.whatViewing)
           .toJS()
-                            // viewing: TODOITEM_ID
     default:
       return state;
   }
 }
 
+
+var todoListNameHolder = "";
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.currentlyViewingChanger = this.currentlyViewingChanger.bind(this);
+  }
+  handleClick(e) {
+    console.log( e.currentTarget.dataset.id )
+    this.currentlyViewingChanger(e.currentTarget.dataset.id )
+  }
+  currentlyViewingChanger(e){
+    store.dispatch({
+        type: 'UPDATECURRENTLYVIEWING', whatViewing: e
+    })
+  }
+  render() {
+    todoListNameHolder = "";
+    todoListNameHolder = this.props.value.map((z) =>
+      <li data-id={z} onClick={this.handleClick.bind(this)}>
+        {z}
+      </li>
+    );
+    return <ul>{todoListNameHolder}</ul>;
+  }
+}
+
+const Counter = ({
+  value,
+  todoAdder,
+  todoRemover,
+  todoItemAdder,
+  todoItemRemover,
+  toggleComplete,
+  currentlyViewingChanger
+}) => (
+  <div>
+    <button onClick={todoAdder}>Add Todo</button>
+    <button onClick={todoRemover}>Remove Todo</button>
+    <button onClick={todoItemAdder}>Add Todo Item</button>
+    <button onClick={toggleComplete}>Toggle Completion</button>
+    <button onClick={todoItemRemover}>Remove Todo Item</button>
+    <button onClick={currentlyViewingChanger}>Change Currently Viewing</button>
+    <h1>{value}</h1>
+  </div>
+);
+
+
+
+const { createStore } = Redux;
+const store = createStore(todoListManager)
+
+const render = () => {
+  ReactDOM.render(
+    [<TodoList
+      value={Object.keys(store.getState().toDoLists)}
+    />,<Counter
+      value={JSON.stringify(store.getState())}
+      todoAdder={() =>
+        store.dispatch({
+          type: 'NEWTODOLIST', name: "todoname"
+        })
+      }
+      todoRemover={() =>
+        store.dispatch({
+           type: 'REMTODOLIST', name: "todoname"
+        })
+      }
+      todoItemAdder={() =>
+        store.dispatch({
+           type: 'ADDTODOITEM', todoListName: "todoname", todoItem: "brush Teeth"
+        })
+      }
+      toggleComplete={() =>
+        store.dispatch({
+           type: 'TOGGLECOMPLETION', todoListName: "todoname", arrayPosition: 0
+        })
+      }
+      todoItemRemover={() =>
+        store.dispatch({
+            type: 'REMTODO', todoListName: "todoname", arrayPosition: 0
+        })
+      }
+      currentlyViewingChanger={() =>
+        store.dispatch({
+            type: 'UPDATECURRENTLYVIEWING', whatViewing: "listYouAreViewing"
+        })
+      }
+    />],
+
+
+
+
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // add todo
 expect(counter(
   {currentViewing:'', toDoLists:{}}, { type: 'NEWTODOLIST', name: "todoname"})
@@ -95,3 +208,4 @@ expect(counter(
 ).toEqual({currentViewing:'listYouAreViewing', toDoLists:{}});
 
 console.log('Test passed');
+*/
